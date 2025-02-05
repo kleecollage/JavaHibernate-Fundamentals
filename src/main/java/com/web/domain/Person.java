120ce3470b60097de0c37ab5be081ef73cb8c9b3
Klee Collage <klee.collage@gmail.com>
2025-02-05 17:32:30 -0600

    feat(domain): Entity Person mapped

--- src/main/java/com/web/domain/Person.java
+++ src/main/java/com/web/domain/Person.java
@@ -18,4 +18,57 @@ public class Person implements Serializable {
     private String surname;
     private String email;
     private String phone;
+
+    public Person() { }
+
+    public int getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(int idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "idPerson=" + idPerson +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
